f70f1dcf294bc4c111f5cb1d17fdf557619a5d72
liyifan <958393320@qq.com>
2019-04-08 23:56:01 +0800

    秒殺功能基本實現

---
+++ src/main/java/com/juzi/access/UserContext.java
@@ -0,0 +1,17 @@
+package com.juzi.access;
+
+import com.juzi.domain.User;
+
+public class UserContext {
+	
+	private static ThreadLocal<User> userHolder = new ThreadLocal<User>();
+	
+	public static void setUser(User user) {
+		userHolder.set(user);
+	}
+	
+	public static User getUser() {
+		return userHolder.get();
+	}
+
+}
